8397ff0c3e4c604916f24bba27ba4c89f1b2e7c4
alperyasit <122600383+alperyasit@users.noreply.github.com>
2025-06-21 23:47:27 +0300

    Update Contact.tsx

--- qr-menu/src/Components/Contact.tsx
+++ qr-menu/src/Components/Contact.tsx
@@ -6,22 +6,34 @@ import SendIcon from "@mui/icons-material/Send";
 function Contact() {
   const contactTextAreaId = useId();
   return (
-    <div className="form-container">
-      <form action="mailto:alperyasit@gmail.com" method="post">
-        <h3>
-          <label htmlFor={contactTextAreaId}>Write your message:</label>
-        </h3>
-        <TextareaAutosize
-          className="textarea"
-          maxRows={6}
-          aria-label="maximum height"
-          placeholder="Write your message here..."
-          defaultValue=""
-        />
-        <Button variant="contained" endIcon={<SendIcon />}>
-          Send
-        </Button>{" "}
-      </form>
+    <div className="contact-container">
+      <div className="form-container">
+        <form action="mailto:alperyasit@gmail.com" method="post">
+          <h3>
+            <label htmlFor={contactTextAreaId}>
+              Your suggestions are valuable to us.
+            </label>
+          </h3>
+          <TextareaAutosize
+            className="textarea"
+            maxRows={6}
+            aria-label="maximum height"
+            placeholder="Write your message here..."
+            defaultValue=""
+          />
+          <Button variant="contained" type="submit" endIcon={<SendIcon />}>
+            Send
+          </Button>
+        </form>
+      </div>
+      <div className="button-container">
+        <Button variant="contained" href="/menu">
+          Back to Menu
+        </Button>
+        <Button variant="contained" href="/">
+          Back to Qr Code
+        </Button>
+      </div>
     </div>
   );
 }
