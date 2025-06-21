0b09fece01cc61f2656ee2206b075d784f762aed
alperyasit <122600383+alperyasit@users.noreply.github.com>
2025-06-21 23:47:25 +0300

    Update Qrcode.tsx

--- qr-menu/src/Components/Qrcode.tsx
+++ qr-menu/src/Components/Qrcode.tsx
@@ -1,12 +1,19 @@
 import frame from "../assets/frame.png";
 import "../App.scss";
+import { Button } from "@mui/material";
 
 function Qrcode() {
   return (
     <div className="qrcode-container">
       <h1>Welcome To Caffeine Lane</h1>
       <img src={frame} alt="" className="transparent-qr" />
-      <p>Scan to View Menu & Social Media</p>
+      <div className="button-container">
+        <h3>Or Click The Button Below</h3>
+        <Button variant="contained" href="/menu">
+          Use a button as a temporary option.{" "}
+        </Button>
+      </div>
+      <p style={{ fontWeight: "bold" }}>Scan to View Menu & Social Media</p>
     </div>
   );
 }
